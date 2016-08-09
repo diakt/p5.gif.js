595bb066f6a0fffc14e1feefb168f76fa60f9554
dryzhov <dryzhov@yandex-team.ru>
2016-08-09 19:57:45 +0300

    play and render

--- examples/sketch.js
+++ examples/sketch.js
@@ -1,20 +1,42 @@
 var gif;
+var gif2;
 var giphy;
+
+var outGif;
+var record = true;
+
+function setupGif() {
+  outGif = new GIF({
+    workers: 2,
+    quality: 40
+  });
+
+  outGif.on('finished', function(blob) {
+    window.open(URL.createObjectURL(blob));
+    setupGif();
+  });
+}
 
 function preload() {
   // var url = 'http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC';
   // giphy = loadJSON(url);
 
   // console.log(giphy)
-  
+  gif = loadGif('test.gif');
+  gif2 = loadGif('test.gif');
+  // jpg = loadImage('code.jpg');
 }
 
+var order = [];
+
+var c;
+
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  c = createCanvas(window.innerWidth, window.innerHeight);
+  setupGif();
   // console.log(giphy)
   // gif = loadGif(giphy.data[0].images.fixed_height.url)
-  gif = loadGif('test.gif');
-  gif.pause();
+
 }
 
 var filters = [
@@ -25,11 +47,19 @@ var filters = [
   'dilate'
 ]
 
+var blendDefault = 'BLEND | DARKEST | LIGHTEST | DIFFERENCE | MULTIPLY| EXCLUSION | SCREEN | REPLACE | OVERLAY | HARD_LIGHT | SOFT_LIGHT | DODGE | BURN | ADD | NORMAL';
+var blends = blendDefault.split(' | ').map(mode => mode.toLowerCase())
+
+
 var stop = false;
 var flag;
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomBlend () {
+
 }
 
 function countWidth() {
@@ -40,10 +70,44 @@ function countHeight() {
 
 }
 var frame;
+var frameDelay;
 var offsetTop = 0;
 var offesetLeft = 0;
 var len;
-function draw() {  
+
+var loaded = false;
+
+function draw() {
+  if (gif.loaded() && gif2.loaded() && !loaded) {
+    len = gif.frames().length;
+    order = _.range(len);
+    order = _.chain(order).map((item, i) => i).shuffle().value();
+    frameDelay = gif.frames()[0].delay;
+    console.log(order)
+    loaded = true;
+  }
+  
+  if (loaded && !stop) {
+    frame = int(frameCount/frameDelay) % len;
+    gif.frame(order[frame]);
+    console.log(`count: ${frame}; len: ${len}; frame: ${order[frame]}`);
+    // gif.filter(filters[getRandomInt(0,3)])
+    
+    image(gif);
+    
+    if (frameCount % 24) {
+      tint(255, getRandomInt(0, 55));
+      image(gif2);
+      if (record) {
+        outGif.addFrame(c.elt, {delay: 1, copy: true});
+      }
+      // blend(gif2, 0, 0, width, height, 0, 0, width, height, blends[getRandomInt(0, blends.length - 1)]);  
+    }
+    
+  }
+}
+
+function drawFrames() {  
   // background(0,0,0);
   if (gif && gif.loaded() && !stop) {
       frame = gif.frame(10);
@@ -104,5 +168,8 @@ function mouseMoved() {
 }
 
 function mousePressed() {
-  stop = true
+  stop = !stop;
+  if (record) {
+    outGif.render();
+  }  
 }
